b91d2441b499abb5c5af9798f597ab435c0564a5
annapgsm <anna.pingsmann@gmail.com>
2025-12-03 13:55:32 +0100

    Feature 1

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,22 @@
-// src/__tests__/App.test.js
+/* eslint-disable no-undef */
 import { render } from '@testing-library/react';
 import React from 'react';
-import App from './../App';
+import App from '../App';
 
 
 describe('<App /> component', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
     
-    test('renders list of events', () => {
-       const AppDOM = render(<App />).container.firstChild;
-       expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-     });
+  test('renders list of events', () => {
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+    
+  /* verifies that the main application correctly renders the search component used for finding cities. */
+  test('render CitySearch', () => {
+    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+  }); 
 
 });
