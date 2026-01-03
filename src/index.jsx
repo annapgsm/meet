bf0208fc36954b98d7a745e82411ef6272272954
annapgsm <anna.pingsmann@gmail.com>
2026-01-03 13:17:01 +0100

    Added Atatus

---
+++ src/index.jsx
@@ -0,0 +1,2 @@
+atatus.config('f63b49ebc0b9430692c66eaa1b807652').install();
+atatus.notify(new Error('Test Atatus Setup'));
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as atatus from 'atatus-spa';
+atatus.config('f63b49ebc0b9430692c66eaa1b807652').install();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
