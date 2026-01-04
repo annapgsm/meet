7e870fdd4c7f9fa259068651e682c988e4ba9d00
annapgsm <anna.pingsmann@gmail.com>
2026-01-04 15:42:13 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,11 +43,13 @@ const App = () => {
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
+    if (!allEvents) return; 
+
+    const filteredEvents = currentCity === "See all cities" 
+      ? allEvents 
+      : allEvents.filter(event => event.location === currentCity);
+    setEvents(filteredEvents?.slice(0, currentNOE) || []);
+    setAllLocations(extractLocations(allEvents) || []);
   };
 
 
